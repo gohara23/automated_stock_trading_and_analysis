ca581e8dc48e66264415fc49d73f63a7ee09917d
gohara23 <59593124+gohara23@users.noreply.github.com>
2020-12-22 22:36:46 -0500

    Ticker list

--- wallstreetbets.py
+++ wallstreetbets.py
@@ -1,22 +1,28 @@
 import praw
-import json 
+import json
+import robin_stocks as rh
+from robin_stocks.helper import error_ticker_does_not_exist
+
+ticker_file = open('tickers.csv', 'r')
+tickers = ticker_file.read().splitlines()
+
 
 keys_filename = 'reddit.json'
 keys_file = open(keys_filename)
 keys = json.load(keys_file)
 
 
-reddit = praw.Reddit(client_id=keys['client_id'] ,
-                    client_secret=keys['secret'] ,
-                    username= keys['username'],
-                    password= keys['password'],
-                    user_agent= 'wsb')
-
+reddit = praw.Reddit(client_id=keys['client_id'],
+                     client_secret=keys['secret'],
+                     username=keys['username'],
+                     password=keys['password'],
+                     user_agent='wsb')
 
 
 subreddit = reddit.subreddit('wallstreetbets')
 
-hot = subreddit.hot(limit=10)
+hot = subreddit.hot(limit=50)
 
 for submission in hot:
-    print(submission.title)
+    if not submission.stickied:
+        print(submission.title)
