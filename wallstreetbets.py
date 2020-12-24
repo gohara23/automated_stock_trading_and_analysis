81da38fe9529e440210b303bf33f3d78916d1846
gohara23 <59593124+gohara23@users.noreply.github.com>
2020-12-24 00:00:45 -0500

    Ticker lists/removed tickers due to freq words

--- wallstreetbets.py
+++ wallstreetbets.py
@@ -1,10 +1,16 @@
+from genericpath import exists
 import praw
 import json
 import robin_stocks as rh
-from robin_stocks.helper import error_ticker_does_not_exist
+import pandas as pd
 
-ticker_file = open('tickers.csv', 'r')
-tickers = ticker_file.read().splitlines()
+
+# ticker_file = open('tickers.csv', 'r')
+# tickers = ticker_file.read().splitlines()
+tickers_df = pd.read_csv('tickers_header.csv')
+tickers_df['count'] = 0
+tickers_df.set_index('ticker', inplace=True)
+
 
 
 keys_filename = 'reddit.json'
@@ -21,8 +27,44 @@ reddit = praw.Reddit(client_id=keys['client_id'],
 
 subreddit = reddit.subreddit('wallstreetbets')
 
-hot = subreddit.hot(limit=50)
+hot = subreddit.hot(limit=5)
 
-for submission in hot:
-    if not submission.stickied:
-        print(submission.title)
+# direct = dir(subreddit)
+# for at in direct:
+#     print(at)
+
+top = subreddit.top('week', limit=5)
+# direct = dir(top)
+# for at in direct:
+#     print(at)
+
+for submission in top:
+    print(submission.title)
+    comments = submission.comments
+    for comment in comments:
+        for comment in comments:
+            try:
+                # print(20*'-')
+                words = comment.body.split()
+                for word in words:
+                    if tickers_df.at[word, 'count'] >= 0:
+                        tickers_df.at[word, 'count'] += 1
+            except:
+                pass
+
+tickers_df.to_csv('ticker_counts.csv')
+tickers_df.sort_values(by=['count'], ascending=False, inplace=True)
+tot = tickers_df['count'].sum()
+tickers_df['pct'] = [item / tot for item in tickers_df['count']]
+print(tickers_df)
+
+# for submission in hot:
+#     if not submission.stickied:
+#         print(submission.title)
+#         comments = submission.comments
+#         for comment in comments:
+#             print(20*'-')
+#             try:
+#                 print(comment.body)
+#             except:
+#                 pass
