724e2e4df45a6671a2aa3bc299f692cfa3f2ab4e
gohara23 <59593124+gohara23@users.noreply.github.com>
2020-12-27 00:55:39 -0500

    Def function for wsb ticker mention weighting

--- wallstreetbets.py
+++ wallstreetbets.py
@@ -1,70 +1,97 @@
-from genericpath import exists
 import praw
 import json
-import robin_stocks as rh
 import pandas as pd
 
 
-# ticker_file = open('tickers.csv', 'r')
-# tickers = ticker_file.read().splitlines()
-tickers_df = pd.read_csv('tickers_header.csv')
-tickers_df['count'] = 0
-tickers_df.set_index('ticker', inplace=True)
+''' Counts mentions of tickers on r/wallstreetbets and for top posts
+    of the week, month, and year. Weights the tickers accordingly '''
 
 
+def get_fund_weights(week_weight=0.25, month_weight=0.25, year_weight=0.5, num_posts=1000):
 
-keys_filename = 'reddit.json'
-keys_file = open(keys_filename)
-keys = json.load(keys_file)
+    tickers_df = pd.read_csv('tickers_header.csv')
+    tickers_df['count_week'] = 0
+    tickers_df['count_month'] = 0
+    tickers_df['count_year'] = 0
+    tickers_df['pct_week'] = 0
+    tickers_df['pct_month'] = 0
+    tickers_df['pct_year'] = 0
+    tickers_df['fund_weights'] = 0
+    tickers_df.set_index('ticker', inplace=True)
 
+    keys_filename = 'reddit.json'
+    keys_file = open(keys_filename)
+    keys = json.load(keys_file)
 
-reddit = praw.Reddit(client_id=keys['client_id'],
-                     client_secret=keys['secret'],
-                     username=keys['username'],
-                     password=keys['password'],
-                     user_agent='wsb')
+    reddit = praw.Reddit(client_id=keys['client_id'],
+                         client_secret=keys['secret'],
+                         username=keys['username'],
+                         password=keys['password'],
+                         user_agent='wsb')
 
+    wsb = reddit.subreddit('wallstreetbets')
 
-subreddit = reddit.subreddit('wallstreetbets')
+    top_week = wsb.top('week', limit=num_posts)
+    top_month = wsb.top('month', limit=num_posts)
+    top_year = wsb.top('year', limit=num_posts)
 
-hot = subreddit.hot(limit=5)
-
-# direct = dir(subreddit)
-# for at in direct:
-#     print(at)
-
-top = subreddit.top('week', limit=5)
-# direct = dir(top)
-# for at in direct:
-#     print(at)
-
-for submission in top:
-    print(submission.title)
-    comments = submission.comments
-    for comment in comments:
+    for submission in top_week:
+        comments = submission.comments
         for comment in comments:
             try:
-                # print(20*'-')
                 words = comment.body.split()
                 for word in words:
-                    if tickers_df.at[word, 'count'] >= 0:
-                        tickers_df.at[word, 'count'] += 1
+                    if tickers_df.at[word, 'count_week'] >= 0:
+                        tickers_df.at[word, 'count_week'] += 1
             except:
                 pass
 
-tickers_df.to_csv('ticker_counts.csv')
-tickers_df.sort_values(by=['count'], ascending=False, inplace=True)
-tot = tickers_df['count'].sum()
-tickers_df['pct'] = [item / tot for item in tickers_df['count']]
-print(tickers_df)
+    for submission in top_month:
+        comments = submission.comments
+        for comment in comments:
+            try:
+                words = comment.body.split()
+                for word in words:
+                    if tickers_df.at[word, 'count_month'] >= 0:
+                        tickers_df.at[word, 'count_month'] += 1
+            except:
+                pass
 
-# for submission in hot:
-#     if not submission.stickied:
-#         print(submission.title)
-#         comments = submission.comments
-#         for comment in comments:
-#             print(20*'-')
-#             try:
-#                 print(comment.body)
-#             except:
-#                 pass
+    for submission in top_year:
+        comments = submission.comments
+        for comment in comments:
+            try:
+                words = comment.body.split()
+                for word in words:
+                    if tickers_df.at[word, 'count_year'] >= 0:
+                        tickers_df.at[word, 'count_year'] += 1
+            except:
+                pass
+
+    tot_week = tickers_df['count_week'].sum()
+    tickers_df['pct_week'] = [
+        item / tot_week for item in tickers_df['count_week']]
+
+    tot_month = tickers_df['count_month'].sum()
+    tickers_df['pct_month'] = [
+        item / tot_month for item in tickers_df['count_month']]
+
+    tot_year = tickers_df['count_year'].sum()
+    tickers_df['pct_year'] = [
+        item / tot_year for item in tickers_df['count_year']]
+
+
+    tickers_df['fund_weights'] = [tickers_df.at[ticker, 'pct_week']*week_weight + tickers_df.at[ticker, 'pct_month']
+                                  * month_weight + tickers_df.at[ticker, 'pct_year']*year_weight for ticker in tickers_df.index]
+
+    tickers_df.sort_values(by=['fund_weights'], ascending=False, inplace=True)
+
+    return tickers_df
+
+
+data = get_fund_weights()
+print(data)
+data.to_csv('fund_weights.csv')
+
+
+
