cb1b041d1400d744181c865e9433033b18d3d616
gohara23 <59593124+gohara23@users.noreply.github.com>
2021-03-04 07:14:02 -0500

    use pd.Timestamp vs datetime

--- sharpe_ratio.py
+++ sharpe_ratio.py
@@ -1,5 +1,6 @@
 
 from numpy.lib.function_base import average
+from pandas.tseries.offsets import YearOffset
 import pandas_datareader as web
 import pandas as pd
 import numpy as np
@@ -13,11 +14,13 @@ class Ticker:
         self.ticker = ticker
         end_date = dt.date.today()
         start_date = end_date - dt.timedelta((years_back+1)*365)
-        self.data = pd.DataFrame(web.DataReader(self.ticker, 'yahoo', start_date, end_date))
-  
-        self.volatility, self.average_return, self.returns = self.volatility_averageReturn(years_back, self.data)
+        self.data = pd.DataFrame(web.DataReader(
+            self.ticker, 'yahoo', start_date, end_date))
 
-    def volatility_averageReturn(self, years_back, ticker_data):
+        self.volatility, self.average_return, self.returns = self.volatility_averageReturn(
+            years_back)
+
+    def volatility_averageReturn(self, years_back):
 
         # Prepares Ticker Data for Sharpe Ratio Calc
 
@@ -25,21 +28,21 @@ class Ticker:
         current_year = end_date.year
         start_date = end_date - dt.timedelta((years_back+1)*365)
 
-        # data = pd.DataFrame(web.DataReader(ticker, 'yahoo', start_date, end_date))
-
         interval_starts = [dt.date(current_year-i, 1, 1)
-                        for i in range(years_back, 0, -1)]
+                           for i in range(years_back, 0, -1)]
         interval_ends = [dt.date(current_year-i, 12, 31)
-                        for i in range(years_back, 0, -1)]
+                         for i in range(years_back, 0, -1)]
         start_prices = np.zeros(len(interval_starts))
         end_prices = np.zeros(len(interval_ends))
         i = 0
         for date in interval_starts:
             while start_prices[i] == 0:
                 try:
-                    start_prices[i] = ticker_data.at[str(interval_starts[i]), 'Adj Close']
+                    start_prices[i] = self.data.at[pd.Timestamp(
+                        interval_starts[i]), 'Adj Close']
                 except:
-                    interval_starts[i] = interval_starts[i] + dt.timedelta(days=1)
+                    interval_starts[i] = interval_starts[i] + \
+                        dt.timedelta(days=1)
 
             i += 1
 
@@ -47,14 +50,14 @@ class Ticker:
         for date in interval_ends:
             while end_prices[i] == 0:
                 try:
-                    end_prices[i] = ticker_data.at[str(interval_ends[i]), 'Adj Close']
+                    end_prices[i] = self.data.at[pd.Timestamp(
+                        interval_ends[i]), 'Adj Close']
                 except:
                     interval_ends[i] = interval_ends[i] - dt.timedelta(days=1)
-
             i += 1
 
         returns = [(end_prices[i]-start_prices[i])/start_prices[i]
-                for i in range(0, len(start_prices))]
+                   for i in range(0, len(start_prices))]
 
         volatility = np.std(returns)
         average_return = np.mean(returns)
@@ -70,7 +73,6 @@ class Portfolio:
         self.volatility = np.std(self.ret)
         self.ave_return = np.mean(self.ret)
         self.sharpe = self.sharpe_ratio()
-    
 
     def returns(self):
         returns = np.zeros(len(self.tickers[0].returns))
@@ -99,7 +101,6 @@ class SharpeSimulation:
         self.weights = self.random_portfolio_weights()
         self.portfolios = self.simulate_portfolios()
 
-
     def random_portfolio_weights(self):
         num_tickers = len(self.tickers)
         weights = np.random.rand(self.n, num_tickers)
@@ -109,22 +110,23 @@ class SharpeSimulation:
                 weights[i, j] = weights[i, j] / tot
         return weights
 
-
     def simulate_portfolios(self):
         volatilities = np.zeros(self.n)
         expected_returns = np.zeros(self.n)
         sharpe_ratios = np.zeros(self.n)
-        portfolios = [Portfolio(self.tickers, weight, self.years_back) for weight in self.weights]
+        portfolios = [Portfolio(self.tickers, weight, self.years_back)
+                      for weight in self.weights]
 
         return portfolios
 
     def plot(self):
         sharpe_ratios = [portfolio.sharpe for portfolio in self.portfolios]
-        expected_returns = [portfolio.ave_return for portfolio in self.portfolios]
+        expected_returns = [
+            portfolio.ave_return for portfolio in self.portfolios]
         volatilities = [portfolio.volatility for portfolio in self.portfolios]
 
-
-        plt.scatter(volatilities, expected_returns, c=sharpe_ratios, marker='s')
+        plt.scatter(volatilities, expected_returns,
+                    c=sharpe_ratios, marker='s')
         plt.style.use('classic')
         # plt.title(title, fontsize=15)
         # rc('font', **{'family': 'serif', 'serif': ['Times']})
@@ -135,7 +137,6 @@ class SharpeSimulation:
         cb.set_label('Sharpe Ratio')
         plt.tight_layout()
         plt.show()
-
 
     def max_sharpe(self):
         max_sr = 0
@@ -148,24 +149,17 @@ class SharpeSimulation:
         return max_sr, weights_final
 
 
-
 if __name__ == '__main__':
 
 
-    tickers = ['SPY', 'QQQ', 'CRM', 'ARKK']
+    tickers = ['SPY', 'QQQ', 'DIA', 'TLT', 'VNQ', 'GLD']
     n = 10000
-    years_back = 6
+    years_back = 5
     tickers = [Ticker(tick, years_back) for tick in tickers]
     sim = SharpeSimulation(tickers, years_back, n)
-
 
     max_sr, best_weights = sim.max_sharpe()
 
     print(max_sr, best_weights)
 
     sim.plot()
-
-
-
-
-    
