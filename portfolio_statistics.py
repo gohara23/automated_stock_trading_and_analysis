88c4f597930f6fa3aaa770b2dc03fe9aa29bbfa2
gohara23 <59593124+gohara23@users.noreply.github.com>
2021-11-07 20:28:09 -0500

    Money weighted returns, ave cash added to account

--- portfolio_statistics.py
+++ portfolio_statistics.py
@@ -1,5 +1,5 @@
 from numpy.core.numeric import NaN
-import robin_stocks as rh
+from robin_stocks import robinhood as rh
 import pandas_datareader as web
 import userFunctions as uf
 from pprint import pprint
@@ -56,6 +56,10 @@ class Portfolio():
             float)
         self.data['close_equity'] = self.data['close_equity'].astype(float)
         self.data['open_equity'] = self.data['open_equity'].astype(float)
+
+        uf.login()
+        self.current_acc_value = float(rh.robinhood.load_portfolio_profile()[
+            'equity'])
 
     def get_benchmark_data(self, benchmark_ticker):
         start_date = self.data.at[0, 'begins_at']
@@ -160,63 +164,94 @@ class Portfolio():
         print(self.sharpe)
         return self.sharpe
 
-        # print(self.bank_transfers['created_at'])
+    def get_benchmark_sharpe(self, start_date, end_date, rfr=0.01):
+        data = self.data.dropna()
+        data.where((data['begins_at'] > start_date) & (
+            data['begins_at'] < end_date), inplace=True)
+        data.dropna(inplace=True)
+        std = np.std(data['benchmark_weekly_returns']) * (52**0.5)
+        ave_returns = np.average(data['benchmark_weekly_returns'])
+        ave_returns_annualized = ave_returns*52
+        self.sharpe = (ave_returns_annualized - rfr) / std
+        print(self.sharpe)
+        return self.sharpe
 
-        # self.dates = []
-        # self.total_deposits = []
-        # self.close_equity = []
-        # self.returns = []
-        # self.pct_returns = []
-        # for ix in self.data.index:
-        #     date = dt.datetime.strptime(self.data.at[ix, 'begins_at'][0:10], "%Y-%m-%d" )
-        #     self.dates.append(date)
-        #     deposit = self.bank_transfers[(self.bank_transfers['created_at'] < date)]['amount'].sum()
-        #     self.total_deposits.append(deposit)
-        #     self.close_equity.append(self.data.at[ix, 'close_equity'])
-        #     self.returns.append(self.data.at[ix, 'close_equity'] - deposit)
-        #     self.pct_returns.append((self.data.at[ix, 'close_equity'] - deposit) / deposit)
-        # self.weekly_returns = [self.returns[i+1] -self.returns[i] for i in range(len(self.dates)-1)]
-        # self.weekly_pct_returns = [self.pct_returns[i+1] -self.pct_returns[i] for i in range(len(self.dates)-1)]
-        # self.weekly_returns.insert(0, 0)
-        # self.weekly_pct_returns.insert(0, 0)
+    def get_average_deposited_cash(self, start=None, end=None):
 
-        # print(self.dates)
-        # print(self.total_deposits)
-        # print(self.close_equity)
-        # print(self.returns)
-        # print(self.pct_returns)
-        # print(self.weekly_returns)
-        # print(self.weekly_pct_returns)
-        # plt.plot(self.total_deposits)
-        # plt.show()
+        ix = self.bank_transfers.index[-1]
+        oldest_deposit_date = self.bank_transfers.at[ix, 'created_at']
 
-        # pprint(self.bank_transfers)
+        if start == None or start < oldest_deposit_date:
+            start = oldest_deposit_date
 
-        # plt.plot(self.data['close_equity'])
-        # plt.show()
+        if end == None:
+            end = dt.datetime.today()
 
-        # uf.login()
-        # bank_transfers = rh.get_bank_transfers()
-        # pprint(bank_transfers)
-        # bank_transfers = pd.DataFrame(bank_transfers)
-        # pprint(bank_transfers)
+        total_days = (end - start).days
+        ave_acc_val = 0
+        for ix in self.bank_transfers.index:
+            if self.bank_transfers.at[ix, 'created_at'] < start:
+                ave_acc_val += float(
+                    self.bank_transfers.at[ix, 'amount'])*total_days
+
+            elif self.bank_transfers.at[ix, 'created_at'] > start and self.bank_transfers.at[ix, 'created_at'] < end:
+                time_effect = (
+                    end - self.bank_transfers.at[ix, 'created_at']).days
+                ave_acc_val += float(
+                    self.bank_transfers.at[ix, 'amount']) * time_effect
+
+        ave_acc_val = ave_acc_val / total_days
+        return ave_acc_val
+
+    def get_money_weighted_returns(self, start=None, end=None):
+        ave_cash = self.get_average_deposited_cash(start, end)
+
+        if start == None:
+            start = self.data.at[0, 'begins_at']
+
+        if end == None:
+            end = dt.datetime.now()
+
+        data = self.data.where(self.data['begins_at'] > start)
+        data = data.where(self.data['begins_at'] < end)
+        data.dropna(inplace=True)
+
+        if end > data.at[data.index[-1], 'begins_at']:
+            final_adj_close = self.current_acc_value
+        else:
+            final_adj_close = float(
+                data.at[data.index[-1], 'adjusted_close_equity'])
+
+        print(final_adj_close)
+        begining_adj_close = float(
+            data.at[data.index[0], 'adjusted_close_equity'])
+        print(begining_adj_close)
+        net_change = final_adj_close - begining_adj_close
+        print(net_change)
+        print(ave_cash)
+        # print(data)
+        # print(self.data)
+        money_weighted_pct_return = (net_change / ave_cash) * 100
+        return money_weighted_pct_return
 
 
 if __name__ == '__main__':
-    # uf.login()
-    # hist = rh.get_historical_portfolio(interval='week', span='all')
-    # print(hist)
-    # df = pd.DataFrame(hist['equity_historicals'])
-    # pprint(df)
     portfolio = Portfolio()
-    portfolio.get_benchmark_data('TSLA')
-    portfolio.get_weekly_returns()
-    vol = portfolio.get_anualized_volatility()
-    portfolio.get_benchmark_weekly_returns()
-    bv = portfolio.get_benchmark_annualized_volatility()
-    print(bv)
-    print(vol)
-    start = dt.datetime(2020, 1, 17)
-    end = dt.datetime(2021, 9, 15)
-    portfolio.get_beta(start, end)
-    portfolio.get_sharp_ratio(start, end)
+    start = dt.datetime(2020, 6, 19)
+    ave_cash = portfolio.get_average_deposited_cash(start=start)
+    print(ave_cash)
+    money_weighted_return = portfolio.get_money_weighted_returns(start)
+    print(money_weighted_return)
+
+    # portfolio.get_benchmark_data('BTC-USD')
+    # portfolio.get_weekly_returns()
+    # vol = portfolio.get_anualized_volatility()
+    # portfolio.get_benchmark_weekly_returns()
+    # bv = portfolio.get_benchmark_annualized_volatility()
+    # print(bv)
+    # print(vol)
+    # start = dt.datetime(2020, 1, 17)
+    # end = dt.datetime(2021, 9, 15)
+    # portfolio.get_beta(start, end)
+    # portfolio.get_sharp_ratio(start, end)
+    # portfolio.get_benchmark_sharpe(start, end)
